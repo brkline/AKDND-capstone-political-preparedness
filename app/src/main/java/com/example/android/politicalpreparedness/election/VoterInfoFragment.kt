a2e99f45f63680d75922066bb6dbaddc56b74181
brkline <brkline@users.noreply.github.com>
2021-07-05 11:04:24 -0400

    Added Styling, Fixed Bugs, and Updated README.md
    Prior to submitting project for review, styling was
    added to the app, testing was done to find and
    fix bugs, and README.md was updated.

--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
